447796c93dd02b978e6c688d0483cde991843ba0
pallxk <github@pallxk.com>
2018-02-24 23:21:28 +0800

    调整图表样式

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
+Highcharts.setOptions({
+  lang: {
+    contextButtonTitle: '导出图表',
+    downloadJPEG: '下载 JPEG 格式图像',
+    downloadPDF: '下载 PDF 格式文档',
+    downloadPNG: '下载 PNG 格式图像',
+    downloadSVG: '下载 SVG 格式图像',
+    printChart: '打印图表',
+    resetZoom: '重置缩放',
+    resetZoomTitle: '重置缩放比例为 1:1',
+
+    thousandsSep: '',
+  }
+});
+
 $.get('delta.csv', function(csv) {
 
 Highcharts.chart('chart', {
+  chart: {
+    zoomType: 'x',
+  },
+  credits: {
+    href: 'http://51hupai.org/',
+    text: '数据来源：51hupai.org'
+  },
   data: {
     csv: csv,
   },
@@ -8,13 +30,19 @@ Highcharts.chart('chart', {
     text: '历年拍牌数据',
   },
   subtitle: {
-    text: '数据来源：51hupai.org',
+    text: 'https://hupai.tips',
+  },
+  tooltip: {
+    shared: true,
+    valuePrefix: '¥',
   },
   yAxis: {
+    min: 0,
+    tickInterval: 100,
     title: {
       text: '警示价差值',
     },
-  }
+  },
 });
 
 });
